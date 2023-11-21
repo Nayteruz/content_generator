1cd82ce3b9d669f19360a867ac2b0e4671c72554
aleksandr.pankov <nayteruz@gmail.com>
2023-11-21 18:42:27 +0700

    add preloader, check lang

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,6 +10,7 @@ import {MenuItems} from "./generate";
 import {SiteSubject} from "./prompt/Subject/SiteSubject";
 import {getContent} from "./api";
 import {IMessage, getMessage, promptMenuList, messagesSiteSubject, fields} from "./prompt";
+import {Loading} from "./loading";
 
 
 
@@ -29,6 +30,7 @@ export const App = () => {
     });
     const [responseSubject, setResponseSubject] = useState('');
     const [isPending, setIsPending] = useState(false);
+    const [lang, setLang] = useState('ru')
 
 
 
@@ -41,11 +43,17 @@ export const App = () => {
     const message: IMessage[] = [
         {role: 'system', content: `Ты профессиональный копирайтер-маркетолог и консультант по созданию контента для веб-сайта. Сайт разрабатывается по тематике ${promptSubject.subject}, вид сайта ${promptSubject.type}, цель создания сайта ${promptSubject.purpose}, тип сайта ${promptSubject.property}. Твоя задача подготовить рекомендованный список и структуру страниц  для этого сайта.`}
     ]
+    const message2: IMessage[] = [
+        {role: 'system', content: `You are a professional copywriter-marketer and content creation consultant for a website. The theme of a website is ${promptSubject.subject}. Website type is ${promptSubject.type}. The purpose of the website is ${promptSubject.purpose}. The website will be ${promptSubject.property}. Your task is to prepare a recommended list and structure of pages for this website based on the information about the site. Provide the response in the form of a structured list of pages in Russian.`}
+    ]
+
+
 
     const getContentInfo = async () => {
         setIsPending(true);
         // const list = getMessage(promptSubject, promptMenuList);
-        const responseDataMessage = await getContent({messages: message});
+        const messages = lang === 'ru' ? message : message2;
+        const responseDataMessage = await getContent({messages});
         setResponseSubject(responseDataMessage);
     }
 
@@ -64,11 +72,16 @@ export const App = () => {
                     {info2}
                 </InfoBlock>
                 <InfoBlock background="#eee" color="#333">
-                    <p>Вы можете сгенерировать наполнение с помощью нашей новой функции генерации контента</p>
+                    <p>Вы можете сгенерировать наполнение с помощью нашей новой функции генерации контента
+                        <select value={lang} onChange={(e) => setLang(e.target.value)}>
+                            <option value="ru">Русский</option>
+                            <option value="en">Английский</option>
+                        </select>
+                    </p>
                     {fields.map((field) => <SiteSubject key={field.id} value={promptSubject} addValue={setPromptSubject} field={field}/>)}
                     <button type="button" onClick={getContentInfo}>Отправить информацию</button>
                     <hr/>
-                    <div>Ответ:  {isPending ? 'Loading...' : responseSubject}</div>
+                    <div className={s.answer}>Ответ:  {isPending ? <Loading/> : responseSubject}</div>
                     <hr/>
                     <MenuItems items={responseSubject} addPage={addItemPage}/>
                     <Pagelist pages={pages}/>
---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,6 @@
+import s from './Loading.module.scss'
+
+export const Loading = () => {
+
+    return <div className={s.loader}></div>
+}
