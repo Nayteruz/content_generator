a5f1b0d8f1d01f4e376e0cf296770ec460bffe80
akbar <akbarsafarov.as@gmail.com>
2023-11-30 18:54:31 +0500

    input

---
+++ src/components/ui/input/Input.tsx
@@ -0,0 +1,38 @@
+import { ChangeEventHandler, FocusEventHandler, MouseEventHandler } from "react";
+import s from "./Input.module.scss";
+
+export type InputProps = {
+  name?: string;
+  onChange?: ChangeEventHandler<HTMLElement>;
+  onClick?: MouseEventHandler<HTMLElement>;
+  onFocus?: FocusEventHandler<HTMLElement>;
+  placeholder?: string;
+  value?: string;
+};
+
+export const Input = ({
+  name,
+  onClick,
+  onChange,
+  onFocus,
+  placeholder,
+  value,
+}: InputProps) => {
+
+  return (
+    <label className={s.inputLabel}>
+      <div className={s.title}>
+        <div className={s.name}>{name}</div>
+      </div>
+      <input
+        type="text"
+        className={s.input}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+        onClick={onClick}
+        onFocus={onFocus}
+      />
+    </label>
+  );
+};
