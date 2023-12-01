11958f8005dff518e7939a6d49da4d85a50d0d2f
akbar <akbarsafarov.as@gmail.com>
2023-12-01 19:22:33 +0500

    fixed upload

--- src/components/uploadFiles/UploadItem.tsx
+++ src/components/uploadFiles/UploadItem.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Icon} from "@/components/ui";
+import MoreIcon from "@/assets/more.svg";
+import {PopupPageAction} from "@/components/popupPageAction";
 import {UploadItemTypes} from "./types";
 import s from './Upload.module.scss';
 
 export const UploadItem = ({name, type, comment, icon }:UploadItemTypes) => {
+  const [togglePopup, setTogglePopup] = useState(false);
   let image = (
     type === 'doc' || type === 'pdf' || type === 'link' ? (
       <Icon icon={type} size="big" />
@@ -19,6 +22,33 @@ export const UploadItem = ({name, type, comment, icon }:UploadItemTypes) => {
       <div className={s.detals}>
         <div className={s.filename}>{name}</div>
         <Button className={s.commentBtn} tag="div" color="color-blue">+ добавить комментарий</Button>
+      </div>
+      <div className={s.fileActions}>
+        <MoreIcon className={s.more} onClick={() => setTogglePopup((prev) => !prev)} />
+        {togglePopup && <PopupPageAction>
+          <ul className={s.list}>
+            <li>
+              <Button
+                className={s.button}
+                tag="div"
+                size="small"
+                appearance="blue"
+              >
+                Переименовать
+              </Button>
+            </li>
+            <li>
+                <Button
+                  className={s.button}
+                  tag="div"
+                  size="small"
+                  appearance="blue"
+                >
+                  Удалить
+                </Button>
+            </li>
+          </ul>
+        </PopupPageAction>}
       </div>
     </div>
   );
